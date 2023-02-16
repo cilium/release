cilium/release

commit 2a086b71ee4ed9aba83a9b8864e8e2cc2879a3d6

Avoid moving pending backports to new project
As part of backport tracking, we set labels on PRs and attempt to ensure
that when a backport is "done" for a branch, the labels are set and this
controls which column in the versioned project that the PR ends up in.

When we do a new release, the previous behaviour of this tool was to
move all "pending" backports to the project for the next release
version. However, sometimes an earlier step is missed, which means that
PRs that have already been backported eg to 1.12.4 are marked as
"pending" in 1.12.4. This tool then mistakenly moves them to
"pending to 1.12.5", which is incorrect and confusing.

In the absence of a better system to control these, at least avoid
propagating the mistake in this tool. By default, prevent the release
manager from moving "pending backports" to a new project. There is an
override to allow moving the pending backports if they are *really*
pending, but hopefully this at least stops the release long enough to
make sure the backports are in the right status in the backport.

Signed-off-by: Joe Stringer <joe@cilium.io>

cmd/projects/projects.go
@@ -158,7 +158,7 @@ func (pm *ProjectManagement) createCard(ctx context.Context, columnID, prID int6
 	return err
 }
 
-func (pm *ProjectManagement) syncCards(ctx context.Context, currVer, nextVer string, currColumnID, nextColumnID, currDoneColumnID, nextPendingColumnID int64) error {
+func (pm *ProjectManagement) syncCards(ctx context.Context, currVer, nextVer string, currColumnID, nextColumnID, currDoneColumnID, nextPendingColumnID int64, forceMovePending bool) error {
 	// get base cards
 	currCards, _, err := pm.ghClient.Projects.ListProjectCards(ctx, currColumnID, &gh.ProjectCardListOptions{})
 	if err != nil {
@@ -200,6 +200,10 @@ func (pm *ProjectManagement) syncCards(ctx context.Context, currVer, nextVer str
 				labelFound = true
 				// If it is pending, them move it to the right column in the new
 				// project.
+				if !forceMovePending {
+					return fmt.Errorf("Found unexpected pending PR https://github.com/%s/%d in project. Please ensure that all backported PRs have been moved to the done column.",
+						repoName, prNmuber)
+				}
 				moveToColumnID = nextPendingColumnID
 				fmt.Fprintf(os.Stdout, "moving PR %d to %q\n", prNumber, columnName(pendingBackportPrefix, nextVer))
 			}
@@ -232,7 +236,7 @@ func NewProjectManagement(ghClient *gh.Client, owner, repo string) *ProjectManag
 	}
 }
 
-func (pm *ProjectManagement) SyncProjects(ctx context.Context, currVer, nextVer string) error {
+func (pm *ProjectManagement) SyncProjects(ctx context.Context, currVer, nextVer string, forceMovePending bool) error {
 	currProjID, nextProjID, err := pm.findProjects(ctx, currVer, nextVer)
 	if err != nil {
 		return err
@@ -302,12 +306,12 @@ func (pm *ProjectManagement) SyncProjects(ctx context.Context, currVer, nextVer 
 	}
 
 	// Move needs backport column cards to the correct columns
-	err = pm.syncCards(ctx, currVer, nextVer, currNeedsColumnID, nextNeedsColumnID, currDoneColumnID, nextPendingColumnID)
+	err = pm.syncCards(ctx, currVer, nextVer, currNeedsColumnID, nextNeedsColumnID, currDoneColumnID, nextPendingColumnID, true)
 	if err != nil {
 		return err
 	}
 	// Move pending backport column cards to the correct columns
-	err = pm.syncCards(ctx, currVer, nextVer, currPendingColumnID, nextPendingColumnID, currDoneColumnID, nextPendingColumnID)
+	err = pm.syncCards(ctx, currVer, nextVer, currPendingColumnID, nextPendingColumnID, currDoneColumnID, nextPendingColumnID, forceMovePending)
 	if err != nil {
 		return err
 	}

cmd/main.go
@@ -56,6 +56,12 @@ var (
 	repoName   string
 	currVer    string
 	nextVer    string
+
+	// forceMovePending lets "pending" backports be moved from one project
+	// to another. By default this is set to false, since most commonly
+	// this is a mistake and the PR should have been previously marked as
+	// "backport-done".
+	forceMovePending bool
 )
 
 func init() {
@@ -66,6 +72,7 @@ func init() {
 	flag.StringVar(&lastStable, "last-stable", "", "When last stable version is set, it will be used to detect if a bug was already backported or not to that particular branch (e.g.: '1.5', '1.6')")
 	flag.StringVar(&stateFile, "state-file", "release-state.json", "When set, it will use the already fetched information from a previous run")
 	flag.StringVar(&repoName, "repo", "cilium/cilium", "GitHub organization and repository names separated by a slash")
+	flag.BoolVar(&forceMovePending, "force-move-pending-backports", false, "Force move pending backports to the next version's project")
 	flag.Parse()
 
 	if len(base) == 0 && len(currVer) == 0 {
@@ -119,7 +126,7 @@ func main() {
 
 	if len(currVer) != 0 {
 		pm := projects.NewProjectManagement(ghClient, owner, repo)
-		err := pm.SyncProjects(globalCtx, currVer, nextVer)
+		err := pm.SyncProjects(globalCtx, currVer, nextVer, forceMovePending)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "Unable to manage project: %s\n", err)
 			os.Exit(-1)

commit 4680f136c7c78890a0baa6058e5d1be7711012ca

fix golang build
It's currently not possible to build this repository. This commit fixes
the build.

cmd/projects/projects.go:205:7: undefined: repoName
cmd/projects/projects.go:205:17: undefined: prNmuber

Fixes: 2a086b71ee4e ("Avoid moving pending backports to new project")
Signed-off-by: André Martins <andre@cilium.io>

cmd/projects/projects.go
@@ -202,7 +202,7 @@ func (pm *ProjectManagement) syncCards(ctx context.Context, currVer, nextVer str
 				// project.
 				if !forceMovePending {
 					return fmt.Errorf("Found unexpected pending PR https://github.com/%s/%d in project. Please ensure that all backported PRs have been moved to the done column.",
-						repoName, prNmuber)
+						pm.repo, prNumber)
 				}
 				moveToColumnID = nextPendingColumnID
 				fmt.Fprintf(os.Stdout, "moving PR %d to %q\n", prNumber, columnName(pendingBackportPrefix, nextVer))