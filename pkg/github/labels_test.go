cilium/release

commit 88e810cd32338a7e34895904402c6b5a66182926

pkg/github: Update code to fetch upstream PRs
After the introduction of the GH workflow Backport Label Updater, there
is no need to manually update the labels of each Backport PR in OSS. The
generated text asking to manually update the labels in the Backport PR
has been removed, so the code to fetch the PRs numbers is updated
accordingly.

Signed-off-by: Fabio Falzoi <fabio.falzoi@isovalent.com>

pkg/github/labels.go
@@ -57,21 +57,8 @@ func getUpstreamPRs(body string) []int {
 	if len(block) == 0 {
 		return nil
 	}
-	// typical block contains something among these lines:
-	// for pr in 9959 9982 10005; do contrib/backporting/set-labels.py $pr done 1.6; done
-	if !strings.Contains(body, "for pr in") {
-		return nil
-	}
-	// blocks may contain a prompt symbol before the "for" loop
-	block = strings.TrimPrefix(block, "$ ")
-	block = strings.TrimPrefix(block, "for pr in")
-	bashLines := strings.Split(block, ";")
-	if len(bashLines) < 1 {
-		return nil
-	}
-	strNumbers := strings.Split(bashLines[0], " ")
 	var prNumbers []int
-	for _, strNumber := range strNumbers {
+	for _, strNumber := range strings.Fields(block) {
 		prNumber, err := strconv.Atoi(strNumber)
 		if err != nil {
 			continue

commit 39c7c26c806e63da99c8eb01647da2e2f1e7adb7

pkg/github: Support v1 and v2 format in getUpstreamPRs
The generated text asking to manually update the labels in the Backport
PRs has been removed and the getUpstreamPRs has been updated accordingly
in a previous commit.

Nevertheless, there will be a window where cilium/release will have to
parse Backport PRs with the "old" (v1) body format alongside Backport
PRs with the "new" (v2) body format.

An example of the v1 body format is this:

```
for pr in 9959 9982 10005; do contrib/backporting/set-labels.py $pr done 1.6; done
```

While the v2 body format is simply a space separated list of backported
PRs:

```
 9959 9982 10005
```

This commit extends the getUpstreamPRs function to look for text
patterns that should be present only in "v1" body format and then try to
parse the block using the parser for the guessed format.

The unit test has been updated to verify the function behaviour with PR
body written in both v1 and v2 formats.

Fixes: 88e810c ("pkg/github: Update code to fetch upstream PRs")

Signed-off-by: Fabio Falzoi <fabio.falzoi@isovalent.com>

pkg/github/labels.go
@@ -57,6 +57,46 @@ func getUpstreamPRs(body string) []int {
 	if len(block) == 0 {
 		return nil
 	}
+
+	// Look for substrings that should be present in a Backport PR body in v1 format
+	if strings.Contains(block, "for pr in") || strings.Contains(block, "contrib/backporting/set-labels.py") {
+		return getUpstreamPRsV1(body, block)
+	}
+
+	// otherwise assume that the body is in v2 format
+	return getUpstreamPRsV2(block)
+}
+
+func getUpstreamPRsV1(body, block string) []int {
+	// v1 of a Backport PR body should follow this format:
+	//
+	// for pr in 9959 9982 10005; do contrib/backporting/set-labels.py $pr done 1.6; done
+	if !strings.Contains(body, "for pr in") {
+		return nil
+	}
+	// blocks may contain a prompt symbol before the "for" loop
+	block = strings.TrimPrefix(block, "$ ")
+	block = strings.TrimPrefix(block, "for pr in")
+	bashLines := strings.Split(block, ";")
+	if len(bashLines) < 1 {
+		return nil
+	}
+	strNumbers := strings.Split(bashLines[0], " ")
+	var prNumbers []int
+	for _, strNumber := range strNumbers {
+		prNumber, err := strconv.Atoi(strNumber)
+		if err != nil {
+			continue
+		}
+		prNumbers = append(prNumbers, prNumber)
+	}
+	return prNumbers
+}
+
+func getUpstreamPRsV2(block string) []int {
+	// v2 of a Backport PR body should follow this format:
+	//
+	// 9959 9982 10005
 	var prNumbers []int
 	for _, strNumber := range strings.Fields(block) {
 		prNumber, err := strconv.Atoi(strNumber)

pkg/github/labels_test.go
@@ -93,13 +93,13 @@ func Test_getBackportPRs(t *testing.T) {
 	type args struct {
 		body string
 	}
-	tests := []struct {
+	testsV1 := []struct {
 		name string
 		args args
 		want []int
 	}{
 		{
-			name: "get all three PRs",
+			name: "get all three PRs in v1 block",
 			args: args{
 				body: "```upstream-prs\r\n$ for pr in 9959 9982 10005; do contrib/backporting/set-labels.py $pr done 1.6; done\r\n```",
 			},
@@ -110,7 +110,7 @@ func Test_getBackportPRs(t *testing.T) {
 			},
 		},
 		{
-			name: "get all three PRs (no prompt symbol)",
+			name: "get all three PRs in v1 block (no prompt symbol)",
 			args: args{
 				body: "```upstream-prs\r\nfor pr in 9959 9982 10005; do contrib/backporting/set-labels.py $pr done 1.6; done\r\n```",
 			},
@@ -121,7 +121,7 @@ func Test_getBackportPRs(t *testing.T) {
 			},
 		},
 		{
-			name: "get single PR",
+			name: "get single PR in v1 block",
 			args: args{
 				body: "```upstream-prs\r\n$ for pr in 9959 ; do contrib/backporting/set-labels.py $pr done 1.6; done\r\n```",
 			},
@@ -130,7 +130,7 @@ func Test_getBackportPRs(t *testing.T) {
 			},
 		},
 		{
-			name: "get single PR (no prompt symbol)",
+			name: "get single PR in v1 block (no prompt symbol)",
 			args: args{
 				body: "```upstream-prs\r\n$ for pr in 9959 ; do contrib/backporting/set-labels.py $pr done 1.6; done\r\n```",
 			},
@@ -139,28 +139,28 @@ func Test_getBackportPRs(t *testing.T) {
 			},
 		},
 		{
-			name: "command line pattern missing",
+			name: "command line pattern missing in v1 block",
 			args: args{
 				body: "```upstream-prs\r\n$ 9 ; do contrib/backporting/set-labels.py $pr done 1.6; done\r\n```",
 			},
 			want: nil,
 		},
 		{
-			name: "command line pattern missing (no prompt symbol)",
+			name: "command line pattern missing in v1 block (no prompt symbol)",
 			args: args{
 				body: "```upstream-prs\r\n9 ; do contrib/backporting/set-labels.py $pr done 1.6; done\r\n```",
 			},
 			want: nil,
 		},
 		{
-			name: "command line pattern incomplete",
+			name: "command line pattern incomplete in v1 block",
 			args: args{
 				body: "```upstream-prs\r\npr in 99 ; do contrib/backporting/set-labels.py $pr done 1.6; done\r\n```",
 			},
 			want: nil,
 		},
 		{
-			name: "unfinished quote section",
+			name: "unfinished quote section in v1 block",
 			args: args{
 				body: "```upstream-prs\n$ for pr in 9959 ; do contrib/backporting/set-labels.py $pr done 1.6; done\r\nfoo\nbar",
 			},
@@ -168,8 +168,44 @@ func Test_getBackportPRs(t *testing.T) {
 				9959,
 			},
 		},
+		{
+			name: "get all three PRs in v2 block",
+			args: args{
+				body: "```upstream-prs\r\n 9959 9982 10005\r\n```",
+			},
+			want: []int{
+				9959,
+				9982,
+				10005,
+			},
+		},
+		{
+			name: "get single PR in v2 block",
+			args: args{
+				body: "```upstream-prs\r\n 9959\r\n```",
+			},
+			want: []int{
+				9959,
+			},
+		},
+		{
+			name: "empty block in v2 block",
+			args: args{
+				body: "```upstream-prs\r\n\r\n```",
+			},
+			want: nil,
+		},
+		{
+			name: "unfinished quote section in v2 block",
+			args: args{
+				body: "```upstream-prs\n 9959\r\nfoo\nbar",
+			},
+			want: []int{
+				9959,
+			},
+		},
 	}
-	for _, tt := range tests {
+	for _, tt := range testsV1 {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := getUpstreamPRs(tt.args.body); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("getUpstreamPRs() = %v, want %v", got, tt.want)