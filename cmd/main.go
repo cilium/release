077879002b2071f7c0f94a891bf4efe948aac6a0
Joe Stringer <joe@cilium.io>
2024-04-02 11:09:46 -0700

    cmd: Restructure using spf13/cobra
    Signed-off-by: Joe Stringer <joe@cilium.io>

--- cmd/main.go
+++ cmd/main.go
@@ -21,35 +21,46 @@ import (
 	"os"
 	"os/signal"
 
-	flag "github.com/spf13/pflag"
-
 	"github.com/cilium/release/cmd/changelog"
 	"github.com/cilium/release/cmd/projects"
 	"github.com/cilium/release/pkg/github"
+
+	"github.com/spf13/cobra"
 )
 
 var (
 	cfg               changelog.Config
 	globalCtx, cancel = context.WithCancel(context.Background())
+
+	rootCmd = &cobra.Command{
+		Use:          "release",
+		Short:        "release -- Prepare a Cilium release",
+		SilenceUsage: true,
+		Run: func(cmd *cobra.Command, args []string) {
+			logger := log.New(os.Stderr, "", 0)
+			if err := cfg.Sanitize(); err != nil {
+				cmd.Usage()
+				logger.Fatalf("\nFailed to validate configuration: %s", err)
+			}
+			go signals()
+			run(logger)
+		},
+	}
 )
 
 func init() {
-	flag.StringVar(&cfg.CurrVer, "current-version", "", "Current version - the one being released")
-	flag.StringVar(&cfg.NextVer, "next-dev-version", "", "Next version - the next development cycle")
-	flag.StringVar(&cfg.Base, "base", "", "Base commit / tag used to generate release notes")
-	flag.StringVar(&cfg.Head, "head", "", "Head commit used to generate release notes")
-	flag.StringVar(&cfg.LastStable, "last-stable", "", "When last stable version is set, it will be used to detect if a bug was already backported or not to that particular branch (e.g.: '1.5', '1.6')")
-	flag.StringVar(&cfg.StateFile, "state-file", "release-state.json", "When set, it will use the already fetched information from a previous run")
-	flag.StringVar(&cfg.RepoName, "repo", "cilium/cilium", "GitHub organization and repository names separated by a slash")
-	flag.BoolVar(&cfg.ForceMovePending, "force-move-pending-backports", false, "Force move pending backports to the next version's project")
-	flag.Parse()
+	addFlags(rootCmd)
+}
 
-	if err := cfg.Sanitize(); err != nil {
-		fmt.Fprintf(os.Stderr, "%s\n", err)
-		flag.Usage()
-		os.Exit(-1)
-	}
-	go signals()
+func addFlags(cmd *cobra.Command) {
+	cmd.Flags().StringVar(&cfg.CurrVer, "current-version", "", "Current version - the one being released")
+	cmd.Flags().StringVar(&cfg.NextVer, "next-dev-version", "", "Next version - the next development cycle")
+	cmd.Flags().StringVar(&cfg.Base, "base", "", "Base commit / tag used to generate release notes")
+	cmd.Flags().StringVar(&cfg.Head, "head", "", "Head commit used to generate release notes")
+	cmd.Flags().StringVar(&cfg.LastStable, "last-stable", "", "When last stable version is set, it will be used to detect if a bug was already backported or not to that particular branch (e.g.: '1.5', '1.6')")
+	cmd.Flags().StringVar(&cfg.StateFile, "state-file", "release-state.json", "When set, it will use the already fetched information from a previous run")
+	cmd.Flags().StringVar(&cfg.RepoName, "repo", "cilium/cilium", "GitHub organization and repository names separated by a slash")
+	cmd.Flags().BoolVar(&cfg.ForceMovePending, "force-move-pending-backports", false, "Force move pending backports to the next version's project")
 }
 
 func signals() {
@@ -60,6 +71,12 @@ func signals() {
 }
 
 func main() {
+	if err := rootCmd.Execute(); err != nil {
+		os.Exit(1)
+	}
+}
+
+func run(logger *log.Logger) {
 	ghClient := github.NewClient(os.Getenv("GITHUB_TOKEN"))
 
 	if len(cfg.CurrVer) != 0 {
@@ -72,7 +89,6 @@ func main() {
 		return
 	}
 
-	logger := log.New(os.Stderr, "", 0)
 	cl, err := changelog.GenerateReleaseNotes(globalCtx, ghClient, logger, cfg)
 	if err != nil {
 		logger.Fatalf("%s\n", err)
