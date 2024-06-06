d2682fb2b58d59336bb5565df748b93dbdba709a
André Martins <andre@cilium.io>
2024-06-06 10:40:57 +0200

    create draft release for target release
    Signed-off-by: André Martins <andre@cilium.io>

--- cmd/release/post-release_pull_request.go
+++ cmd/release/post-release_pull_request.go
@@ -4,10 +4,13 @@
 package release
 
 import (
+	"bufio"
 	"context"
 	"fmt"
 	"io"
 	"os"
+	"path/filepath"
+	"regexp"
 	"strings"
 
 	"github.com/cilium/release/pkg/github"
@@ -31,6 +34,85 @@ func (pc *PustPostPullRequest) Name() string {
 }
 
 func (pc *PustPostPullRequest) Run(ctx context.Context, yesToPrompt, dryRun bool, ghClient *gh.Client) error {
+	// Generate release summary
+	changelogFile := filepath.Join(pc.cfg.RepoDirectory, "CHANGELOG.md")
+	changelogContent, err := os.Open(changelogFile)
+	if err != nil {
+		if !os.IsNotExist(err) {
+			return fmt.Errorf("error reading CHANGELOG.md file: %w", err)
+		} else {
+			return fmt.Errorf("CHANGELOG.md file not found, it needs to be present to create a release on GitHub")
+		}
+	}
+	defer changelogContent.Close()
+
+	digestFileName := fmt.Sprintf("digest-%s.txt", pc.cfg.TargetVer)
+	digestFile := filepath.Join(pc.cfg.RepoDirectory, digestFileName)
+	digestFileContent, err := os.Open(digestFile)
+	if err != nil {
+		if !os.IsNotExist(err) {
+			return fmt.Errorf("error reading %s file: %w", digestFileName, err)
+		} else {
+			return fmt.Errorf("%s file not found, it needs to be present to create a release on GitHub", digestFileName)
+		}
+	}
+	defer digestFileContent.Close()
+
+	releaseSummaryFileName := fmt.Sprintf("%s-release-summary.txt", pc.cfg.TargetVer)
+	releaseSummaryFile := filepath.Join(pc.cfg.RepoDirectory, releaseSummaryFileName)
+	releaseSummaryFileContent, err := os.Create(releaseSummaryFile)
+	if err != nil {
+		return fmt.Errorf("unable to create summary file: %w", err)
+	}
+	defer releaseSummaryFileContent.Close()
+
+	detectVersion := regexp.MustCompile(`^## v.*$`)
+	scanner := bufio.NewScanner(changelogContent)
+	for i := 0; scanner.Scan(); i++ {
+		// Ignore the first four lines
+		if i < 4 {
+			continue
+		}
+		// Stop when we hit the previous version in the CHANGELOG.md
+		if detectVersion.Match(scanner.Bytes()) {
+			break
+		}
+		releaseSummaryFileContent.Write(append(scanner.Bytes(), byte('\n')))
+	}
+
+	if _, err := io.Copy(releaseSummaryFileContent, digestFileContent); err != nil {
+		return fmt.Errorf("Unable to copy the digest file conent into the release summary file")
+	}
+	releaseSummaryFileContent.Close()
+
+	var preRelease string
+	if semver.Prerelease(pc.cfg.TargetVer) != "" {
+		preRelease = "-p"
+	}
+
+	ersion := strings.TrimPrefix(pc.cfg.TargetVer, "v")
+	_, err = execCommand(pc.cfg.RepoDirectory,
+		"gh",
+		"release",
+		"create",
+		"--draft",
+		preRelease,
+		"--notes-file",
+		releaseSummaryFileName,
+		pc.cfg.TargetVer,
+		"--title",
+		ersion,
+	)
+
+	if err != nil {
+		return err
+	}
+
+	if !pc.cfg.HasStableBranch() {
+		io2.Fprintf(1, os.Stdout, "Pre-Releases don't need to have a post pull request done "+
+			"as we don't commit the digests.\n")
+		return nil
+	}
 	io2.Fprintf(1, os.Stdout, "📤 Submitting changes to a PR\n")
 
 	baseBranch := semver.MajorMinor(pc.cfg.TargetVer)
