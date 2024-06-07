cilium/release

commit cc1751db35a016556d3720da2b9c711048b26df0

contrib/release: Switch from 'hub' to 'gh'
[ cherry-picked from cilium/cilium repository ]

Signed-off-by: Joe Stringer <joe@cilium.io>
Signed-off-by: Quentin Monnet <qmo@qmon.net>

internal/submit-release.sh
@@ -29,8 +29,8 @@ if ! git branch -a | grep -q "$REMOTE/$BRANCH$" || [ ! -e $SUMMARY ]; then
     exit 1
 fi
 
-if ! hub help | grep -q "pull-request"; then
-    echo "This tool relies on 'hub' from https://github.com/github/hub." 1>&2
+if ! gh help | grep -q "pr"; then
+    echo "This tool relies on 'gh' from https://cli.github.com/." 1>&2
     echo "Please install this tool first." 1>&2
     exit 1
 fi
@@ -67,4 +67,4 @@ LABELS="kind/release"
 if [ "$BRANCH" != "main" ]; then
     LABELS="$LABELS,backport/$(echo $BRANCH | sed 's/^v//')"
 fi
-hub pull-request -b "$BRANCH" -l "$LABELS" -F $SUMMARY
+gh pr create -b "$BRANCH" -l "$LABELS" -F $SUMMARY -t "$(head -n 1 $SUMMARY)"

internal/post-release.sh
@@ -28,8 +28,8 @@ handle_args() {
         common::exit 0
     fi
 
-    if ! hub help | grep -q "pull-request"; then
-        echo "This tool relies on 'hub' from https://github.com/github/hub." 1>&2
+    if ! gh help > /dev/null; then
+        echo "This tool relies on 'gh' from https://cli.github.com/." 1>&2
         echo "Please install this tool first." 1>&2
         common::exit 1
     fi
@@ -66,13 +66,12 @@ main() {
     git checkout -b pr/$version-digests $version
     ${DIR}/pull-docker-manifests.sh "$@"
 
-    echo -e "$ersion\n" > $version-release-summary.txt
     # Grab the release notes for the current release, stop before the next
     # release. The start of line `## vX.Y.Z` lines will match in command.
-    tail -n+4 CHANGELOG.md | sed '/^## v.*$/q' >> $version-release-summary.txt
+    tail -n+4 CHANGELOG.md | sed '/^## v.*$/q' > $version-release-summary.txt
     tail -n+2 digest-$version.txt >> $version-release-summary.txt
     logecho "Creating Github draft release"
-    logrun hub release create -d -F $version-release-summary.txt $version
+    logrun gh release create -d $prerelease -F $version-release-summary.txt $version --title "$ersion"
     logecho "Browse to $RELEASES_URL to see the draft release"
 
     if version_is_prerelease "$version" && ! version_is_rc "$version"; then
@@ -95,7 +94,7 @@ main() {
     logecho "Sending pull request for branch v$branch..."
     PR_BRANCH=$(git rev-parse --abbrev-ref HEAD)
     git push $user_remote "$PR_BRANCH"
-    hub pull-request -b "v$branch" -l backport/$branch
+    gh pr create -B "v$branch" -l backport/$branch
 }
 
 main "$@"

internal/tag-release.sh
@@ -27,8 +27,8 @@ handle_args() {
         common::exit 1 "VERSION file not found. Is this directory a Cilium repository?"
     fi
 
-    if ! which hub >/dev/null; then
-        echo "This tool relies on 'hub' from https://github.com/github/hub ." 1>&2
+    if ! which gh >/dev/null; then
+        echo "This tool relies on 'gh' from https://cli.github.com/ ." 1>&2
         common::exit 1 "Please install this tool first."
     fi
 

commit 97b09a8d4770fd64426adffa63298ca00417c5b7

internal/submit-release
Fix flag in PR creation. When we refactored hub with gh, the flag should
have also been renamed from `-b` to `-B`.

Fixes: cc1751db35a0 ("contrib/release: Switch from 'hub' to 'gh'")
Signed-off-by: André Martins <andre@cilium.io>

internal/submit-release.sh
@@ -67,4 +67,4 @@ LABELS="kind/release"
 if [ "$BRANCH" != "main" ]; then
     LABELS="$LABELS,backport/$(echo $BRANCH | sed 's/^v//')"
 fi
-gh pr create -b "$BRANCH" -l "$LABELS" -F $SUMMARY -t "$(head -n 1 $SUMMARY)"
+gh pr create -B "$BRANCH" -l "$LABELS" -F $SUMMARY -t "$(head -n 1 $SUMMARY)"