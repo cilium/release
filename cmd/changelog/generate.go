cilium/release

commit f56f58f7b409759a5b51d42f5c4f802b6a45e90e

Include repo name in release note entries
Include repo name in release note entries so that pull request links
render properly when pull requests and releases are in different repos.
For example Hubble CLI pull requests are in cilium/cilium, and releases
are in cilium/hubble. Rest assured this does not change how release note
entries get rendered in GitHub release page, i.e. GitHub release page
only shows pull request numbers without the repository name.

Signed-off-by: Michi Mutsuzaki <michi@isovalent.com>

cmd/changelog/generate.go
@@ -191,7 +191,7 @@ func (cl *ChangeLog) PrintReleaseNotes() {
 
 			changelogItems = append(
 				changelogItems,
-				fmt.Sprintf("* %s (#%d, @%s)", pr.ReleaseNote, prID, pr.AuthorName),
+				fmt.Sprintf("* %s (%s#%d, @%s)", pr.ReleaseNote, cl.RepoName, prID, pr.AuthorName),
 			)
 			delete(cl.listOfPrs, prID)
 		}
@@ -222,7 +222,7 @@ func (cl *ChangeLog) PrintReleaseNotes() {
 			}
 			changelogItems = append(
 				changelogItems,
-				fmt.Sprintf("* %s (#%d, @%s)", pr.ReleaseNote, prID, pr.AuthorName),
+				fmt.Sprintf("* %s (%s#%d, @%s)", pr.ReleaseNote, cl.RepoName, prID, pr.AuthorName),
 			)
 			delete(cl.listOfPrs, prID)
 		}

commit 9ad0a9c1e273bd7ddb7d9f73194a74bbd3cd9ee6

Include repo name in backport PR entries
I forgot to handle this case in #198.

Fixes: f56f58f7b409 ("Include repo name in release note entries")

Signed-off-by: Michi Mutsuzaki <michi@isovalent.com>

cmd/changelog/generate.go
@@ -310,7 +310,7 @@ func (cl *ChangeLog) prReleaseNote(pr types.PullRequest, prNumber int, upstreamP
 	text := fmt.Sprintf("* %s", pr.ReleaseNote)
 	if !cl.ExcludePRReferences {
 		if upstreamPRNumber != nil {
-			text += fmt.Sprintf(" (Backport PR #%d, Upstream PR #%d, @%s)", prNumber, *upstreamPRNumber, pr.AuthorName)
+			text += fmt.Sprintf(" (Backport PR %s#%d, Upstream PR %s#%d, @%s)", cl.RepoName, prNumber, cl.RepoName, *upstreamPRNumber, pr.AuthorName)
 		} else {
 			text += fmt.Sprintf(" (%s#%d, @%s)", cl.RepoName, prNumber, pr.AuthorName)
 		}